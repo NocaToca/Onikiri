b3ffe610ffa2b882dc5ea8b4f215b8028668633c
NocaToca <66977504+NocaToca@users.noreply.github.com>
2023-03-22 20:45:04 -0400

    You can visualize hitboxes

--- Assets/Scripts/ActionCollider.cs
+++ Assets/Scripts/ActionCollider.cs
@@ -7,6 +7,10 @@ public class ActionCollider : MonoBehaviour
 {
     public enum ColliderType{Box, Circle};
 
+    public ColliderType type;
+
+    public bool debug_display;
+
     CircleCollider2D circle;
     BoxCollider2D box;
 
@@ -23,6 +27,56 @@ public class ActionCollider : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnDrawGizmos(){
+        if(debug_display){
+
+            System.Func<Vector3, Vector2, Vector3> apply_offset = delegate(Vector3 world_position, Vector2 offset){
+                world_position.x += offset.x;
+                world_position.y += offset.y;
+                return world_position;
+            };
+
+            if(type == ColliderType.Box){
+                if(box == null){
+                    box = GetComponent<BoxCollider2D>();
+                }
+                Vector2 size_vector = box.size;
+                Vector2 offset = box.offset;
+
+                Vector3 world_position = this.transform.position;
+                world_position = apply_offset(world_position, offset);
+
+                if(is_player_in_action_collider){
+                    Gizmos.color = Color.green;
+                } else {
+                    Gizmos.color = Color.red;
+                }
+
+                Gizmos.DrawWireCube(world_position, new Vector3(size_vector.x, size_vector.y, 1.0f));
+            } else 
+            if(type == ColliderType.Circle){
+                if(circle == null){
+                    circle = GetComponent<CircleCollider2D>();
+                }
+
+                float radius = circle.radius;
+                Vector2 offset = circle.offset;
+
+                Vector3 world_position = this.transform.position;
+                world_position = apply_offset(world_position, offset);
+
+                if(is_player_in_action_collider){
+                    Gizmos.color = Color.green;
+                } else {
+                    Gizmos.color = Color.red;
+                }
+
+                Gizmos.DrawWireSphere(world_position, radius);
+            }
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
